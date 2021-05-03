0f0739b216d675d80a526b654ba2c7adf3282018
haluk <i.halukaksoy@gmail.com>
2021-05-04 00:01:32 +0300

    comments added to the timemanager script
    why do we use this script, which has no other function but keep track of time to initiate create and destroy news objects? basically everything that has anything to do with the newsfeed logic is moved into the news script to ease debugging and maintenance in the future. I believe that the sole reason of this scripts' existence should remain with time related issues

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -7,9 +7,16 @@ public class TimeManager : MonoBehaviour
 {
     //todo: save time to be continued by on the next launch
 
+    //3.05.2021 newsfeed codebase update
+    //why do we use this script, which has no other function but keep track of time to initiate create and destroy news objects?
+    //basically everything that has anything to do with the newsfeed logic is moved into the news script to ease debugging and maintenance in the future
+    //i believe that the sole reason of this scripts' existence should remain with time related issues
+
     public double elapsedTime;
     public Text TimeText;
     public int displayTime;
+
+    [SerializeField] int secondsPerTurn = 10; // how many seconds do we want to pass before going to the next round?
 
     private void Start()
     {
@@ -20,7 +27,7 @@ public class TimeManager : MonoBehaviour
     {
         elapsedTime += Time.deltaTime;
 
-        if((int)elapsedTime % 10 == 0)
+        if((int)elapsedTime % secondsPerTurn == 0)
         {
             showTime();
         }
@@ -29,7 +36,7 @@ public class TimeManager : MonoBehaviour
 
     private void showTime()
     {
-        displayTime = (int)(elapsedTime / 10) + 1;
+        displayTime = (int)(elapsedTime / secondsPerTurn) + 1;
         TimeText.text = displayTime.ToString();
     }
 }
