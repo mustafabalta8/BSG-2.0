45e67e62862ce66a70160689c7412187df9a84bf
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2021-05-18 22:50:14 +0300

    AssignEmp-Acceptcontract-db connections, dissmiss emp. after duration

--- Assets/Scripts/AcceptContract.cs
+++ Assets/Scripts/AcceptContract.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Data;
 using TMPro;
 public class AcceptContract : MonoBehaviour
@@ -13,16 +14,23 @@ public class AcceptContract : MonoBehaviour
     public int code;
     public int art;
     public int design;
+    public int ContID;
 
     Company company;
     DbManager dbManager;
     CreateContract createContract;
+    MoneyManager moneyManager;
+    TimeManager timeManager;
 
     [SerializeField] GameObject AssignEmpObj;
     [SerializeField] GameObject AssignEmpPanel;
 
     [SerializeField] GameObject SelectedContractObj;
-   // [SerializeField] GameObject SelectedContractPanel;
+
+    public List<int> AssignedEmployees = new List<int>();
+    [SerializeField] Slider ProcessSld;
+    
+    // [SerializeField] GameObject SelectedContractPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +38,51 @@ public class AcceptContract : MonoBehaviour
         company = FindObjectOfType<Company>();
         dbManager = FindObjectOfType<DbManager>();
         createContract = FindObjectOfType<CreateContract>();
+        moneyManager = FindObjectOfType<MoneyManager>();
+        timeManager = FindObjectOfType<TimeManager>();
         ShowEmployeesInAssignPanel();
+
+    }
+    public void TakeContract()
+    {
+        //ProcessSld.maxValue = duration;
+        Debug.Log("Contract Accepted");
+        for (int i=0;i< AssignedEmployees.Count; i++)
+        {
+        string query = $"INSERT INTO employees_asign_to_tasks VALUES ({AssignedEmployees[i]},{0},{ContID})";
+
+            dbManager.ReadRecords(query);
+        }
+        string query02 = $"INSERT INTO taken_contracts VALUES ({ContID},\"{platform}\",\"{sofType}\",{timeManager.displayTime},{duration},{offer},{code},{design},{art})";
+        dbManager.ReadRecords(query02);
+        dbManager.CloseConnection();
+
+        createContract.DestroyContract(ContID);//Can't remove RectTransform because Image (Script), Image (Script), VerticalLayoutGroup (Script), VerticalLayoutGroup (Script), VerticalLayoutGroup (Script) depends on it
+
+        StartCoroutine(EndProduction(ContID, AssignedEmployees));
+
+
+
+
+
+    }
+    IEnumerator EndProduction(int ContID,List<int> EmpId)
+    {
+        Debug.Log("Coroutine Started");
+        yield return new WaitForSeconds(duration* timeManager.secondsPerTurn);
+        // dismiss employees  / earn money / 
+        moneyManager.changeMoney(offer,"Contract");
+
+        string query = string.Format("DELETE FROM employees_asign_to_tasks  WHERE con_id ='{0}'", ContID);
+        dbManager.DeleteRecords(query);
+        for(int i =0; i < EmpId.Count; i++)
+        {
+            string query02 = string.Format("UPDATE employees SET busy ='0' WHERE employeeId='{0}'", EmpId[i]);
+            dbManager.InsertRecords(query02);
+        }
+        
+        dbManager.CloseConnection();
+        Debug.Log("Contract Deleted/Finished");
 
     }
     public void ShowEmployeesInAssignPanel()
@@ -99,6 +151,8 @@ public class AcceptContract : MonoBehaviour
         }
         createContract.CreateContractUI(contractObj);
     }
+
+
 
     
 }
