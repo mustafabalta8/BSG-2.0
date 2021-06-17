777ffa6d4d069444a4bb45849d1f82aeb479592d
haluk <i.halukaksoy@gmail.com>
2021-06-17 20:08:53 +0300

    skill buy complete

--- Assets/Scripts/SkillTree/Skill.cs
+++ Assets/Scripts/SkillTree/Skill.cs
@@ -3,38 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Data;
+
 public class Skill : MonoBehaviour
 {
-    public string SkillName;
-    public string FeatureName;
+    public string skillName, featureName, type;
 
-    public int level;
-    public int cost;
-    public int feature;
+    public int level, cost, feature;
+
+    public bool active = false;
+   int boughtLevel = 0;
+
+    [SerializeField] Button skillButton;
 
     SkillTreeOperator skillTreeOperator;
+    DbManager dbManager;
 
     [SerializeField] GameObject SkillInspector;
     // Start is called before the first frame update
     void Start()
     {
         skillTreeOperator = FindObjectOfType<SkillTreeOperator>();
+        dbManager = FindObjectOfType<DbManager>();
+
+        skillName = this.name;
+        type = this.transform.parent.name;
+
+        fillButton();
+    }
+
+    public void fillButton()
+    {
+        bool bought = false;
+
+        string query = $"SELECT count(level) as nActive, level FROM skills WHERE skillName = \"{skillName}\" AND active = 1 ORDER BY level DESC LIMIT 1";
+        IDataReader reader = dbManager.ReadRecords(query);
+
+        while (reader.Read())
+        {
+            if(reader.GetInt32(0) > 0)
+            {
+                bought = true;
+                boughtLevel = reader.GetInt32(1);
+            }
+        }
+        dbManager.CloseConnection();
+
+        if (bought)
+        {
+            query = $"SELECT level FROM skills WHERE skillName = \"{skillName}\" AND active = 1 ORDER BY level DESC LIMIT 1";
+            reader = dbManager.ReadRecords(query);
+
+            while (reader.Read())
+            {
+                if (boughtLevel == 3)
+                {
+                    skillButton.interactable = false;
+                    this.transform.Find("level").GetComponent<Text>().text = $"Level: 3";
+                }
+                else
+                {
+                    this.transform.Find("level").GetComponent<Text>().text = $"Level: {reader.GetInt32(0)+1}";
+                }
+            }
+            dbManager.CloseConnection();
+        }
+        else
+        {
+            this.transform.Find("level").GetComponent<Text>().text = $"Level: 1";
+        }
     }
 
     public void showInfo()
     {
-        
-        SkillInspector.transform.Find("Skill name").GetComponent<TextMeshProUGUI>().text = SkillName + " Level "+level;
-        SkillInspector.transform.Find("Panel/Furniture").GetComponent<TextMeshProUGUI>().text =FeatureName;
-        SkillInspector.transform.Find("Panel/Cost").GetComponent<TextMeshProUGUI>().text = "Cost: "+cost;
+        int cost = 0;
 
+        string query = $"SELECT * FROM skills WHERE skillName = \"{skillName}\" AND level = {boughtLevel+1} ORDER BY level DESC LIMIT 1";
+        IDataReader reader = dbManager.ReadRecords(query);
 
-        skillTreeOperator.SkillName = SkillName;
-        skillTreeOperator.level = level;
+        while (reader.Read())
+        {
+            SkillInspector.transform.Find("Skill name").GetComponent<TextMeshProUGUI>().text = skillName + $" Level {boughtLevel+1}";
+            SkillInspector.transform.Find("Panel/Furniture").GetComponent<TextMeshProUGUI>().text = $"Service: {reader.GetString(4)}";
+            SkillInspector.transform.Find("Panel/Cost").GetComponent<TextMeshProUGUI>().text = $"Cost: ${reader.GetInt32(6)}";
+            cost = reader.GetInt32(6);
+        }
+        dbManager.CloseConnection();
+
         skillTreeOperator.cost = cost;
-        skillTreeOperator.feature = feature;
-
+        skillTreeOperator.skillName = skillName;
+        skillTreeOperator.skillType = type;
+        skillTreeOperator.level = boughtLevel + 1;
+        skillTreeOperator.skill = this;
     }
-
-    
 }
--- Assets/Scripts/SkillTree/SkillTreeOperator.cs
+++ Assets/Scripts/SkillTree/SkillTreeOperator.cs
@@ -3,166 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Data;
+using UnityEngine.UI;
 
 public class SkillTreeOperator : MonoBehaviour
 {
+    public int level, cost;
+    public string skillName, skillType;
+    public Skill skill;
+    [SerializeField] Button buyButton;
 
-    public string SkillName;
-    public int level;
-    public int cost;
-    public int feature;
-    public string featureName;
 
     DbManager dbManager;
     MoneyManager moneyManager;
+    Notifications notificationManager;
 
     [SerializeField] GameObject SkillsPanel;
 
-     string SkillName0;
-     int level0;
-     int cost0;
-     int feature0;
-     string featureName0;
-
-    // Start is called before the first frame update
     void Start()
     {
-        dbManager = FindObjectOfType<DbManager>();
         moneyManager = FindObjectOfType<MoneyManager>();
-
-        //arrangeSkillsToLevel1();
-
-       DownloadSkillInfo();
-
-
+        notificationManager = FindObjectOfType<Notifications>();
     }
-    void arrangeSkillsToLevel1()//for restart
+
+    public void buySkill()
     {
-        
-
-        string query = "SELECT * FROM skills WHERE level='1'";
-        IDataReader reader8 = dbManager.ReadRecords(query);
-
-        while (reader8.Read())
+        if(moneyManager.getBalance() < cost)
         {
-
-            string skillName = reader8.GetString(0);
-            
-            switch (skillName)
-            {
-                case "thrifty":
-                    fillSkillDetails0( reader8,"thrifty");
-                    break;
-                case "chafferer":
-                    fillSkillDetails0(reader8, "chafferer");
-                    break;
-                case "decorator":
-                    fillSkillDetails0(reader8, "decorator");
-                    break;
-                case "fertile":
-                    fillSkillDetails0(reader8, "fertile");
-                    break;
-                case "peaceful atmosphere":
-                    fillSkillDetails0(reader8, "peaceful atmosphere");
-                    break;
-
-                default:
-                    break;
-            }
-        
-        }
-        dbManager.CloseConnection();
-    }
-    void fillSkillDetails0(IDataReader reader, string nameSkill)
-    {
-
-            level0 = reader.GetInt32(1);
-            featureName0 = reader.GetString(2);
-            feature0 = reader.GetInt32(3);
-            cost0 = reader.GetInt32(4);
-
-        dbManager.CloseConnection();
-        string query02 = string.Format("UPDATE presented_skills SET level ='{0}', featureName='{1}',feature='{2}',cost='{3}' WHERE skillName= '{4}'", level0, featureName0, feature0, cost0,nameSkill);
-        dbManager.InsertRecords(query02);
-    }
-    void DownloadSkillInfo()
-    {
-        string query = "SELECT * FROM presented_skills";
-        IDataReader reader = dbManager.ReadRecords(query);
-
-        while (reader.Read())
-        {
-
-            string skillName = reader.GetString(0);
-
-            switch (skillName)
-            {
-                case "thrifty":
-                    fillSkillDetails(1, reader);
-                    break;
-                case "chafferer":
-                    fillSkillDetails(2, reader);
-                    break;
-                case "decorator":
-                    fillSkillDetails(3, reader);
-                    break;
-                case "fertile":
-                    fillSkillDetails(4, reader);
-                    break;
-                case "peaceful atmosphere":
-                    fillSkillDetails(5, reader);
-                    break;
-
-                default:
-                    break;
-            }
-
-        }
-        dbManager.CloseConnection();
-    }
-
-    public void LevelUpgrade()
-    {
-        //if(cost<=money)
-        //moneyManager.changeMoney(-cost,SkillName);
-
-        //activate the feature
-
-
-        //read next level 
-        //string query = $"SELECT * FROM skills WHERE skillName = {SkillName} AND level = {level+1}";
-        string query = string.Format("SELECT * FROM skills WHERE skillName = '{0}' AND level = {1}", SkillName, level+1);
-        IDataReader reader = dbManager.ReadRecords(query);
-        while (reader.Read())
-        {
-            level = reader.GetInt32(1);
-            featureName = reader.GetString(2);
-            feature = reader.GetInt32(3);
-            cost = reader.GetInt32(4);
+            notificationManager.pushNotification("You don't enough money to buy this skill.");
+            return;
         }
 
-        //change the presented_skilss table in DB
-        //string query02 = $"UPDATE presented_skills SET level ={level}, featureName={featureName},feature={feature},cost={cost} WHERE skillName={SkillName}";
-        string query02 = string.Format("UPDATE presented_skills SET level ='{0}', featureName='{1}',feature='{2}',cost='{3}' WHERE skillName='{4}'",level,featureName,feature,cost,SkillName);
-        dbManager.InsertRecords(query02);
+        try
+        {
+            dbManager = FindObjectOfType<DbManager>();
+            string query = $"UPDATE skills SET active = 0 WHERE skillName = \"{skillName}\"; UPDATE skills SET active = 1 WHERE skillName = \"{skillName}\" AND level = {level};";
+            dbManager.InsertRecords(query);
+            dbManager.CloseConnection();
 
-        dbManager.CloseConnection();
+            notificationManager.pushNotification($"{skillName} level {level} is activated.");
+            skill.fillButton();
 
-
-        DownloadSkillInfo();
-    }
-
-    private void fillSkillDetails(int skillName, IDataReader reader)
-    {
-        SkillsPanel.transform.Find($"Office/Skill {skillName}").GetComponent<Skill>().SkillName = reader.GetString(0);
-        SkillsPanel.transform.Find($"Office/Skill {skillName}").GetComponent<Skill>().level = reader.GetInt32(1);
-        SkillsPanel.transform.Find($"Office/Skill {skillName}").GetComponent<Skill>().FeatureName = reader.GetString(2);
-        SkillsPanel.transform.Find($"Office/Skill {skillName}").GetComponent<Skill>().feature = reader.GetInt32(3);
-        SkillsPanel.transform.Find($"Office/Skill {skillName}").GetComponent<Skill>().cost = reader.GetInt32(4);
-    }
-
-   public void ChangeInspectorInterface()
-    {
+            buyButton.interactable = false;
+        }
+        catch (System.Exception error)
+        {
+            Debug.Log(error);
+            throw;
+        }
 
     }
 }
